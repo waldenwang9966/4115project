a44847fa62257b02aa0941e5fdad4f75396222bf
waldenwang9966 <waldenwang4@gmail.com>
2022-04-12 00:04:35 -0400

    updated

--- app.py
+++ app.py
@@ -13,4 +13,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html', jobs=jobs)
+    return render_template("index.html", jobs=jobs)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
