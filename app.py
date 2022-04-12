7465c696dfb573f9d32df0f0754dd211f461ed2b
waldenwang9966 <waldenwang4@gmail.com>
2022-04-11 23:41:12 -0400

    Added some front page

---
+++ app.py
@@ -0,0 +1,16 @@
+jobs = [
+    {"title": "Software Engineer", "date": "04-01-2022"},
+    {"title": "TeleMarketer", "date": "04-03-2022"},
+    {"title": "Financial Advisor", "date": "04-07-2022"},
+    {"title": "Intern", "date": "04-03-2022"},
+]
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html', jobs=jobs)
